63fe7175faf0e8378f7ef0e11f84da5b03b5c387
Thalles Yan Santos Medrado <ysm.thalles@gmail.com>
2019-08-24 22:49:31 -0300

    modify gcd

--- uva/10104.cpp
+++ uva/10104.cpp
@@ -30,34 +30,69 @@ inline int mod(int n, int m){
 }
 
 /**
- * Euclidian GCD.
+ * Extended Euclidian GCD.
  *
  * Time Complexity: O(log(min(a, b))).
  */
-int gcd(int a, int b, int &x, int &y){
-    if(a == 0){
-        x = 0;
-        y = 1;
-        return b;
+pair<int, ii> gcd(int a, int b){
+    if(a == 0) return {b, {0, 1}};
+    pair<int, ii> res = gcd(b%a, a);
+    int x1 = res.ss.ff, y1 = res.ss.ss;
+    return {res.ff, {y1 - b/a*x1, x1}};
+}
+
+/**
+ * Euclidian derivated LCM.
+ *
+ * Time Complexity: O(log(min(a, b))).
+ */
+int lcm(int a, int b){
+    // same as a*b/gcd(a, b) but avoiding overflow.
+    return a/gcd(a, b).ff*b;
+}
+
+/**
+ * Binary Exponentiation.
+ *
+ * Time Complexity: O(log(e)).
+ */
+int bexp(int b, int e){
+    int res = 1;
+    while(e > 0){
+        if(e & 1)
+            res *= b;
+        b *= b;
+        e >>= 1;
     }
-    int x1, y1, res;
-    res = gcd(b%a, a, x1, y1);
-    x = y1 - b/a*x1;
-    y = x1;
+    return res;
+}
+
+/**
+ * Binary Exponentiation with mod.
+ *
+ * Time Complexity: O(log(e)).
+ */
+int bexpm(int b, int e, int m = M){
+    b %= m;
+    int res = 1;
+    while(e > 0){
+        if(e & 1)
+            res = (res*b)%m;
+        b = (b*b)%m;
+        e >>= 1;
+    }
     return res;
 }
 
 int32_t main(){
     desync();
-    int a, b, x, y, d;
+    int a, b;
     while(cin >> a >> b){
-        d = gcd(a, b, x, y);
-        if(a == b){
-            int aux = x;
-            x = min(x, y);
-            y = max(aux, y);
-        }
-        cout << x << ' ' << y << ' ' << d << endl;
+        auto res = gcd(a, b);
+        if(a == b and res.ss.ff > res.ss.ss)
+            swap(res.ss.ff, res.ss.ss);
+        cout << res.ss.ff << ' ' << res.ss.ss << ' ' << res.ff << endl;
+        assert(a*res.ss.ff + b*res.ss.ss == res.ff);
     }
     return 0;
 }
