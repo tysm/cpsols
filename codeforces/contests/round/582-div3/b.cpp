c45602ffb3460a6d801d79295e8a8e43bdfaf10f
Thalles Yan Santos Medrado <ysm.thalles@gmail.com>
2019-08-30 23:44:10 -0300

    582-div3

---
+++ codeforces/contests/round/582-div3/b.cpp
@@ -0,0 +1,103 @@
+#include <bits/stdc++.h>
+#define int long long
+#define double long double
+using namespace std;
+
+using ii = pair<int, int>;
+using vi = vector<int>;
+using vii = vector<ii>;
+
+#define ff first
+#define ss second
+#define pb push_back
+
+#define EPS 1e-9
+#define INF (int32_t(1e9)+1)
+#define MAX (int32_t(1e6)+1)
+#define M (int32_t(1e9)+7)
+
+#define endl "\n"
+#define debug(x) (cerr << #x << " = " << (x) << endl)
+#define desync() (ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL))
+
+inline int mod(int n, int m){
+    int ret = n%m; 
+    if(ret < 0) ret += m;
+    return ret;
+}
+
+/**
+ * Extended Euclidian GCD.
+ *
+ * Time Complexity: O(log(min(a, b))).
+ */
+pair<int, ii> gcd(int a, int b){
+    if(a == 0) return {b, {0, 1}};
+    pair<int, ii> res = gcd(b%a, a);
+    int x1 = res.ss.ff, y1 = res.ss.ss;
+    return {res.ff, {y1 - b/a*x1, x1}};
+}
+
+/**
+ * Euclidian derivated LCM.
+ *
+ * Time Complexity: O(log(min(a, b))).
+ */
+int lcm(int a, int b){
+    // same as a*b/gcd(a, b) but avoiding overflow.
+    return a/gcd(a, b).ff*b;
+}
+
+/**
+ * Binary Exponentiation.
+ *
+ * Time Complexity: O(log(e)).
+ */
+int bexp(int b, int e){
+    int res = 1;
+    while(e > 0){
+        if(e & 1)
+            res *= b;
+        b *= b;
+        e >>= 1;
+    }
+    return res;
+}
+
+/**
+ * Binary Exponentiation with mod.
+ *
+ * Time Complexity: O(log(e)).
+ */
+int bexpm(int b, int e, int m = M){
+    b %= m;
+    int res = 1;
+    while(e > 0){
+        if(e & 1)
+            res = (res*b)%m;
+        b = (b*b)%m;
+        e >>= 1;
+    }
+    return res;
+}
+
+int32_t main(){
+    desync();
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        vi v(n);
+        for(int i=0; i<n; ++i)
+            cin >> v[i];
+        int ans = 0, aux = INF;
+        for(int i=n-1; i>=0; --i){
+            if(v[i] > aux)
+                ans++;
+            aux = min(v[i], aux);
+        }
+        cout << ans << endl;
+    }
+    return 0;
+}
